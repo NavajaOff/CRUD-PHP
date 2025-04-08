f60836c452245b985b6dd5f330cdb9cb8aeee1f1
Ronald <ronaldbejarano147@.com>
2025-04-07 23:25:10 -0500

    funcion de crear usuario y listar usuario

--- Usuario.php
+++ Usuario.php
@@ -4,7 +4,28 @@ require_once 'database.php';
 
 class Usuario
 {
+    private $primer_nombre;
+    private $segundo_nombre;
+    private $primer_apellido;
+    private $segundo_apellido;
+    private $fecha_nacimiento;
+    private $telefono;
+    private $correo;
+    private $direccion;
+
     private PDO $conn;
+
+
+    public function setDatos($datos) {
+        $this->primer_nombre = $datos['primer_nombre'];
+        $this->segundo_nombre = $datos['segundo_nombre'];
+        $this->primer_apellido = $datos['primer_apellido'];
+        $this->segundo_apellido = $datos['segundo_apellido'];
+        $this->fecha_nacimiento = $datos['fecha_nacimiento'];
+        $this->telefono = $datos['telefono'];
+        $this->correo = $datos['correo'];
+        $this->direccion = $datos['direccion'];
+    }
 
     public function __construct()
     {
@@ -13,6 +34,13 @@ class Usuario
 
     public function listarUsuarios()
     {
+        $sql= "SELECT * FROM usuarios";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        print_r ($usuarios);
+        return $usuarios;
         // Lógica para obtener todos los usuarios
     }
 
@@ -23,6 +51,10 @@ class Usuario
 
     public function crearUsuario()
     {
+        $sql= "INSERT INTO usuarios (primer_nombre, segundo_nombre,primer_apellido,segundo_apellido,fecha_nacimiento,telefono,correo,direccion)
+        VALUES (?,?,?,?,?,?,?,?)";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute([$this->primer_nombre, $this->segundo_nombre, $this->primer_apellido, $this->segundo_apellido, $this->fecha_nacimiento, $this->telefono, $this->correo, $this->direccion]);
         // Lógica para insertar usuario 
     }
 
